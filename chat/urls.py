47852324b04801677837850a15cdf7cf821cbb04
dadakhon09 <odadaxon27@mail.ru>
2019-09-03 17:41:41 +0500

    adsasdasda

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from chat.views import Index, Room
+from chat.views import Index, RoomView
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
-    path('<str:room_name>/', Room.as_view(), name='room'),
+    path('<int:receiver_id>/', RoomView.as_view(), name='room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,20 +1,36 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.utils.safestring import mark_safe
 import json
 
 from django.views import View
 
+from chat.models import Room
+
 
 class Index(View):
     def get(self, request):
         users = User.objects.exclude(username=request.user.username)
-        return render(request, 'index.html', {'users': users})
+        return render(request, 'index.html', {
+            'users': users
+        })
 
 
-class Room(View):
-    def get(self, request, room_name):
-    	return render(request, 'room.html', {
+class RoomView(View):
+    def get(self, request, receiver_id):
+        users = User.objects.exclude(username=request.user.username)
+
+        receiver = get_object_or_404(User, id=receiver_id)
+        if receiver != request.user:
+            room, _ = Room.objects.get_or_create(sender=request.user, receiver=receiver,
+                                                 room_name=f'to-{receiver}')
+        else:
+            return HttpResponse("You can't chat to yourself")
+
+        return render(request, 'room.html', {
+            'users': users,
             'sender': mark_safe(json.dumps(self.request.user.username)),
-            'room_name_json': mark_safe(json.dumps(room_name))
+            'receiver': receiver,
+            'room_name_json': mark_safe(json.dumps(room.room_name))
         })
